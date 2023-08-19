d7cb9d54f93d09322698ddae8602bb9fe6d4a1f9
saadghori <saadghori33@gmail.com>
2023-08-18 21:31:03 -0400

    formatting & comments

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
 
 console.log("Hello from app.js!");
-
+/* Enabling radio buttons */
 document.addEventListener("DOMContentLoaded", function () {
     var hireRadio = document.getElementById("hiring");
     var hourlyContainer = document.getElementById("hourly-rate-container");
-
+    // hides hourly rate input by default, only shown when checked
     function toggleHourlyRateInput() {
         if (hireRadio.checked) {
             hourlyContainer.style.display = "block";
@@ -21,3 +21,4 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+    
